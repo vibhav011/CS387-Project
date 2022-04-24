5f9de144e993a259ba4f5d8f0d4499d2d1c767c3
vibhav011 <vibhav200601@gmail.com>
2022-04-25 02:33:36 +0530

    Structs for query processing

---
+++ receiver/query.cpp
@@ -0,0 +1,5 @@
+#include "query.h"
+
+Temp_Table* execute_select(string table_name, vector<string>col_names, AST* cond_tree) {
+    // Call Table_Scan()
+}
---
+++ receiver/query.h
@@ -0,0 +1,34 @@
+#include "../dblayer/tbl.h"
+
+#ifndef _QRY_H_
+#define _QRY_H_
+#include <vector>
+#include <map>
+#include <string>
+using namespace std;
+
+typedef string Entry;
+
+struct Table_row {
+    vector<Entry> fields;
+    Entry getField(int i) {
+        return fields[i];
+    }
+};
+
+struct Temp_Table {
+    Schema* schema;
+    vector<Table_row*> rows;
+};
+
+struct Log_entry {
+    Table_row *old_value, *new_value;       // NULL indicates value does not exist
+    enum {UPDATE, INSERT, DELETE} change_type;
+};
+
+typedef map<int, Log_entry> ChangeLog;      // map from unique_id to Log_Entry
+typedef map<int, int> MappingLog;           // map from unique_id to record_id
+
+Temp_Table* execute_select(string table_name, vector<string>col_names, AST* cond_tree);
+
+#endif
