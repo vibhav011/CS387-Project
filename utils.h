6990cf07be7f05640d1090ff1a3b7cd1744d3336
vibhav011 <vibhav200601@gmail.com>
2022-04-26 01:09:39 +0530

    Edited utils

--- utils.h
+++ utils.h
@@ -3,22 +3,33 @@
 #include<iostream>
 using namespace std;
 
-union field_type
-{
+typedef union {
     int int_val;
-    double double_val;
-    string string_val;
+    string* str_val;
+    double float_val;
+} Entry;
+
+typedef struct {
+    char *name;
+    int  type;  // one of VARCHAR, INT, LONG
+} ColumnDesc;
+
+typedef struct {
+    int numColumns;
+    ColumnDesc **columns; // array of column descriptors
+} Schema;
+
+struct Table_row {
+    vector<Entry> fields;
+    Entry getField(int i) {
+        return fields[i];
+    }
 };
 
-struct Schema
-{
-    int num_cols;
-    vector<string> col_names;
-    int *dts;
+struct Temp_Table {
+    Schema* schema;
+    vector<Table_row*> rows;
 };
 
-struct Table
-{
-    Schema *schema;
-    vector<vector<string> > table_data;
-};
+int execute_select(Temp_Table *result, vector<string> table_names, vector<string>col_names, AST* cond_tree);
+AST::check_condition(Table_row *tr1, Table_row *tr2);   // tr2 = NULL in case select is not a join
