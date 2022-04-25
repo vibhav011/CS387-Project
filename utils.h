9301994957968e9dd2a8da2253e68e8e6660b75a
akj0811 <jain.ankit201108@gmail.com>
2022-04-26 01:33:33 +0530

    added some funcs to temp_table

--- utils.h
+++ utils.h
@@ -27,9 +27,23 @@ struct Table_row {
 };
 
 struct Temp_Table {
+    string name;
     Schema* schema;
     vector<Table_row*> rows;
+
+    void set_schema_columns(vector<string> names)
+    {
+        for(int i=0;i<names.size();i++)
+            schema->columns[i]->name = &(names[i])[0];
+    }
+
+    void set_name(string name)
+    {
+        name = name;
+    }
 };
+
+# define table_list vector<Tabl*>
 
 int execute_select(Temp_Table *result, vector<string> table_names, vector<string>col_names, AST* cond_tree);
 AST::check_condition(Table_row *tr1, Table_row *tr2);   // tr2 = NULL in case select is not a join
