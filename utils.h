c6fa812b2362c79cf5fb42276d60be674117957e
vibhav011 <vibhav200601@gmail.com>
2022-04-26 17:25:09 +0530

    Small change in utils

--- utils.h
+++ utils.h
@@ -87,7 +87,7 @@ typedef map<int, int> MappingLog;
 map<string, int> table_name_to_id;
 vector<Table*> tables;              // objects of all tables
 vector<int> UIds;                   // constanstly increasing uids for each of the tables
-vector<ChangeLog*> change_logs;     // objects of change logs for corresponding tables in `tables`
-vector<MappingLog*> mapping_logs;
+vector<ChangeLog> change_logs;     // objects of change logs for corresponding tables in `tables`
+vector<MappingLog> mapping_logs;
 
 #endif
