c09bed3615e444bc98bd5fd651de0fc6b84c392a
Tulip Pandey <tulip@Tulips-MacBook-Air.local>
2022-04-26 15:49:18 +0530

    cleaned up headers

--- receiver/query.h
+++ receiver/query.h
@@ -7,29 +7,6 @@
 #include <string>
 using namespace std;
 
-typedef union {
-    int int_val;
-    string* str_val;
-    double float_val;
-} Entry;
-
-struct Table_row {
-    vector<Entry> fields;
-    Entry getField(int i) {
-        return fields[i];
-    }
-};
-
-struct Temp_Table {
-    Schema* schema;
-    vector<Table_row*> rows;
-};
-
-struct Log_entry {
-    Table_row *old_value, *new_value;       // NULL indicates value does not exist
-    enum {UPDATE, INSERT, DELETE} change_type;
-};
-
 struct Query_Obj {
     vector<string> *col_names;
     AST *cond_tree;
@@ -46,20 +23,7 @@ struct Update_pair {
     string rhs;
 };
 
-typedef map<int, Log_entry> ChangeLog;      // map from unique_id to Log_Entry
-typedef map<int, int> MappingLog;           // map from unique_id to record_id
 
-vector<ChangeLog> ChangeLogs;
-
-int num_tables = 0;         // constantly increasing integer for keeping track of number of tables
-
-map<string, int> TableNum;          // map from table name to table number
-
-vector<int> UIds;           // constanstly increasing uids for each of the tables
-
-Temp_Table* execute_select(string table_name, vector<string>col_names, AST* cond_tree);
 int execute_select(Temp_Table *result, vector<string> table_names, vector<string>col_names, AST* cond_tree);
-
-enum {C_OK, C_TRUE, C_FALSE, C_ERROR, C_TABLE_NOT_FOUND, C_FIELD_NOT_FOUND} error_codes;
 
 #endif
--- utils.h
+++ utils.h
@@ -12,15 +12,21 @@ typedef union {
     double float_val;
 } Entry;
 
-typedef struct {
-    char *name;
-    int  type;  // one of VARCHAR, INT, LONG
-} ColumnDesc;
+// typedef struct {
+//     char *name;
+//     int  type;  // one of VARCHAR, INT, LONG
+// } ColumnDesc;
 
-typedef struct {
-    int numColumns;
-    ColumnDesc **columns; // array of column descriptors
-} Schema;
+// typedef struct {
+//     int numColumns;
+//     ColumnDesc **columns; // array of column descriptors
+// } Schema;
+
+struct Log_entry {
+    Table_row *old_value, *new_value;       // NULL indicates value does not exist
+    enum {UPDATE, INSERT, DELETE} change_type;
+};
+
 
 struct Table_row {
     vector<Entry> fields;
@@ -51,11 +57,16 @@ struct Temp_Table {
 int execute_select(Temp_Table *result, vector<string> table_names, vector<string>col_names, AST* cond_tree);
 AST::check_condition(Table_row *tr1, Table_row *tr2);   // tr2 = NULL in case select is not a join
 
+enum {C_OK, C_TRUE, C_FALSE, C_ERROR, C_TABLE_NOT_FOUND, C_FIELD_NOT_FOUND} error_codes;
+
 /////////////////////////////
 // Shared global variables //
 /////////////////////////////
+typedef map<int, Log_entry> ChangeLog;
+typedef map<int, int> MappingLog;
 
 map<string, int> table_name_to_id;
 vector<Table*> tables;              // objects of all tables
+vector<int> UIds;                   // constanstly increasing uids for each of the tables
 vector<ChangeLog*> change_logs;     // objects of change logs for corresponding tables in `tables`
 vector<MappingLog*> mapping_logs;
